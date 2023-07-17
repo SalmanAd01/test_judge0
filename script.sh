d4b0c1f1a8d5dec41e1b4d19148ec9c74811830b
SalmanAd01 <salmanadhikari415@gmail.com>
2023-07-17 22:05:33 +0530

    created shell docker

--- script.sh
+++ script.sh
@@ -10,6 +10,9 @@ apt install -y docker.io docker-compose
 # Step 2: Download and extract the Judge0 release archive
 wget https://github.com/judge0/judge0/releases/download/v1.13.0/judge0-v1.13.0.zip
 unzip judge0-v1.13.0.zip
+sleep 5s
+ls -la
+sleep 5s
 cd judge0-v1.13.0
 
 # Step 3: Run all services and wait for initialization
