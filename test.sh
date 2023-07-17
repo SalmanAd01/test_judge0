f9430e4b0b3223572abdb08b4ddf008789acb1a4
Salman Adhikari <80933048+SalmanAd01@users.noreply.github.com>
2023-07-17 21:16:43 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Step 1: Install Docker and Docker Compose
+# Make sure to update the package manager commands based on your server's operating system.
+
+# Example for Ubuntu/Debian-based systems:
+sudo apt update
+sudo apt install -y docker.io docker-compose
+
+# Step 2: Download and extract the Judge0 release archive
+wget https://github.com/judge0/judge0/releases/download/v1.13.0/judge0-v1.13.0.zip
+unzip judge0-v1.13.0.zip
+cd judge0-v1.13.0
+
+# Step 3: Run all services and wait for initialization
+docker-compose up -d db redis
+sleep 10s
+docker-compose up -d
+sleep 5s
